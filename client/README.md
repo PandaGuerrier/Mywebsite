60959de9d6f9cef8f67aff6fd8d348c7ad0175b6
pandaguerrier <juleslofficial@gmail.com>
2025-09-09 16:38:00 +0200

    feat: refactored all the code

--- client/README.md
+++
@@ -1,1 +0,0 @@
-full auth using adonisjs as backend and nextjs as frontend
--- server/app/middleware/silent_auth_middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-import type { NextFn } from '@adonisjs/core/types/http'
-
-export default class SilentAuthMiddleware {
-  async handle(ctx: HttpContext, next: NextFn) {
-    await ctx.auth.use("web").check()
-    return next()
-  }
-}
